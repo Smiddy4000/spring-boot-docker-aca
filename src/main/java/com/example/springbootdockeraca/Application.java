22691623fbae63655243dcbc739c8be719f22298
Richard Smith <admin@MngEnvMCAP731175.onmicrosoft.com>
2025-03-26 13:12:33 +1100

    Adding new image to the gallery

--- src/main/java/com/example/springbootdockeraca/Application.java
+++ src/main/java/com/example/springbootdockeraca/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
   @RequestMapping("/")
   public String home() {
-    return "Hello Docker World";
+    return "Hello World from Dockerized Spring Boot Application";
   }
 
   public static void main(String[] args) {
