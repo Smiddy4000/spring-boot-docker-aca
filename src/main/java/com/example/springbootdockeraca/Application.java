00323fce04a4b169e4a139d589731e9c3eff2692
Smiddy4000 <richard.smith@microsoft.com>
2025-04-14 14:48:00 +1000

    Demoing my changes

--- src/main/java/com/example/springbootdockeraca/Application.java
+++ src/main/java/com/example/springbootdockeraca/Application.java
@@ -13,7 +13,7 @@ public class Application {
   public String home() {
     return "<html>" +
          "  <head>" +
-         "    <title>iag on Microsoft Azure</title>" +
+         "    <title>Microsoft Azure</title>" +
          "    <link rel=\"stylesheet\" type=\"text/css\" href=\"/styles.css\">" +
          "  </head>" +
          "  <body>" +
@@ -26,11 +26,7 @@ public class Application {
          "          <img src= 'https://uhf.microsoft.com/images/microsoft/RE1Mu3b.png' className='logo react' alt='Microsoft logo' />" +
          "        </a>" +
          "      </div><br />" +
-         "      <div className='logo-container'>" +
-         "        <a href='https://iag.com.au' target='_blank'>" +
-         "           <img src= 'https://www.iag.com.au/sites/default/themes/iag2015/build/images/iag_logo_secondary.png' className='logo' alt='IAG logo' />" +
-         "        </a>" +
-         "      </div><br />" +
+         
          "      <div className='card'>" +
          "        <p>This is a demonstration of a Azure Container App for Java SpringBoot.</p>" +
          "      </div>" +
